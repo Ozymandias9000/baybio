ffc6a4f88b6cb290c0714afe1afae088352d1224
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-09-08 10:53:46 -0700

    Moved styles into one folder, added stylus-normalize

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -48,7 +48,7 @@ export default class SignIn extends Component {
           <input
             type="submit"
             value="Sign In"
-            className="round-button round-button--submit"
+            className="round-button round-button--sign-in"
           />
         </form>
       </div>
