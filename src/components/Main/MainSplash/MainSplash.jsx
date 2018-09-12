2a28f5c511e195519150303fd5d9f3da360c7f07
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-09-11 17:21:42 -0700

    Added textappear to splash page

--- src/components/Main/MainSplash/MainSplash.jsx
+++ src/components/Main/MainSplash/MainSplash.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "@reach/router";
+import textAppear from "../../CustomFuncs/textAppear";
 
 export default class MainSplash extends Component {
+  componentDidMount() {
+    const h1Text = "Baybio";
+    const h3Text = "A baby's story";
+
+    const h1Target = document.querySelector(".splash-banner-text--h1");
+
+    const h3Target = document.querySelector(".splash-banner-text--h3");
+
+    textAppear(h1Text, h1Target, 200, 200);
+    textAppear(h3Text, h3Target, 100, 2000);
+  }
+
   render() {
     return (
       <main className="flex-container--center">
         <div className="splash-banner-text">
-          <h1 className="splash-banner-text--h1">Baybio</h1>
-          <h3 className="splash-banner-text--h3">Your baby's story</h3>
+          <h1 className="splash-banner-text--h1"> </h1>
+          <h3 className="splash-banner-text--h3"> </h3>
           <div className="splash-button--container">
             <Link to="register" className="round-button round-button--splash">
               Register
